5c781b8a72088956c254ec01196f2f044694db11
KyryloStroiev <stroievkyrylo@gmail.com>
2022-11-19 16:18:15 +0200

    Update Program.cs

--- HW16/Program.cs
+++ HW16/Program.cs
@@ -3,14 +3,16 @@ using HW16;
 using System.Text;
 
 Console.OutputEncoding = Encoding.UTF8;
-var rate = TimeSpan.FromMilliseconds(150);
 var game = new Game();
+
+
 
 using (var token = new CancellationTokenSource())
 {
     var check = CheckKeyPresses(token);
     do
     {
+        var rate = TimeSpan.FromMilliseconds(game._rate);
         game.OnTick();
         game.Render();
         await Task.Delay(rate);
@@ -21,9 +23,9 @@ using (var token = new CancellationTokenSource())
 
 async Task CheckKeyPresses(CancellationTokenSource cts)
 {
-    while(!cts.Token.IsCancellationRequested)
+    while (!cts.Token.IsCancellationRequested)
     {
-        if(Console.KeyAvailable)
+        if (Console.KeyAvailable)
             game.OnKeyPress(Console.ReadKey(true).Key);
         await Task.Delay(10);
     }
